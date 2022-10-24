2d3e4fba3ac4f425d7c712a2107e1c444a3400d5
Aritra Biswas <40533894+pandalearnstocode@users.noreply.github.com>
2022-10-24 06:20:04 +0000

    tests: adding parameterized tests for hello_world.

--- tests/test_example.py
+++ tests/test_example.py
@@ -3,8 +3,22 @@ import pytest
 from sample_components.example import hello, hello_world
 
 
-def test_hello_world():
-    assert hello_world("aritra") == "Hello aritra."
+@pytest.mark.parametrize(
+    ("name", "expected"),
+    [
+        ("Jeanette", "Hello Jeanette."),
+        ("Raven", "Hello Raven."),
+        ("Maxine", "Hello Maxine."),
+        ("Matteo", "Hello Matteo."),
+        ("Destinee", "Hello Destinee."),
+        ("Alden", "Hello Alden."),
+        ("Mariah", "Hello Mariah."),
+        ("Anika", "Hello Anika."),
+        ("Isabella", "Hello Isabella."),
+    ],
+)
+def test_hello_world(name, expected):
+    assert hello_world(name) == expected
 
 
 @pytest.mark.parametrize(
