8ea2c8b27c4bd65f6c591d279c75355137f5afe0
Aritra Biswas <pandalearnstocode@gmail.com>
2022-10-26 02:54:22 +0530

    updating dependencies.

--- sample_components/example.py
+++ sample_components/example.py
@@ -1,7 +1,3 @@
-def hello_world(name: str) -> str:
-    return f"Hello {name}."
-
-
 def hello(name: str) -> str:
     """Just an greetings example.
     Args:
@@ -13,20 +9,3 @@ def hello(name: str) -> str:
         'Hello Roman!'
     """
     return f"Hello {name}!"
-
-
-def sample_docstring(name: str) -> str:
-    """This is a sample docstring
-
-    Args:
-        name (str): this is the arg which says who has created this docstring
-
-    Returns:
-        str: this is echo who has created the docstring
-
-
-    Examples:
-        >>> sample_docstring("Roman")
-        'This is a docstring create by - Roman!'
-    """
-    return f"This is a docstring create by - f{name}."
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,24 +1,6 @@
 import pytest
 
-from sample_components.example import hello, hello_world
-
-
-@pytest.mark.parametrize(
-    ("name", "expected"),
-    [
-        ("Jeanette", "Hello Jeanette."),
-        ("Raven", "Hello Raven."),
-        ("Maxine", "Hello Maxine."),
-        ("Matteo", "Hello Matteo."),
-        ("Destinee", "Hello Destinee."),
-        ("Alden", "Hello Alden."),
-        ("Mariah", "Hello Mariah."),
-        ("Anika", "Hello Anika."),
-        ("Isabella", "Hello Isabella."),
-    ],
-)
-def test_hello_world(name, expected):
-    assert hello_world(name) == expected
+from sample_components.example import hello
 
 
 @pytest.mark.parametrize(
