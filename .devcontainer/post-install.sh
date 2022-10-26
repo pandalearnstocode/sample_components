9ef3d77c4a87d1a9190a5ec4eb445c50513c68ec
Aritra Biswas <pandalearnstocode@gmail.com>
2022-10-26 09:13:43 +0530

    updating dependencies.

---
+++ .devcontainer/post-install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -ex
+
+##
+## Create some aliases
+##
+echo 'alias ll="ls -alF"' >> $HOME/.bashrc
+echo 'alias la="ls -A"' >> $HOME/.bashrc
+echo 'alias l="ls -CF"' >> $HOME/.bashrc
+
+# Convenience workspace directory for later use
+WORKSPACE_DIR=$(pwd)
+
+##
+## Python project configuration & deps
+##
+
+# Change some Poetry settings to better deal with working in a container
+poetry config cache-dir ${WORKSPACE_DIR}/.cache
+poetry config virtualenvs.in-project true
+
+# Now install all dependencies
+poetry lock -n
+poetry export --without-hashes > requirements.txt
+poetry install -n
+poetry run pre-commit install
+echo "Done!"
